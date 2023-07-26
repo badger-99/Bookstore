cc3c6b46f1ba4f593340d3e93ffacd31af8e4d5b
Alfred Peter <alpeter91@gmail.com>
2023-07-26 20:06:33 +0300

    Render book form

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,4 +1,7 @@
 const BookForm = () => (
-  <div>BookForm</div>
+  <form>
+    <input type="text" name="title" id="" placeholder="Title" />
+    <input type="text" name="author" id="" placeholder="Author" />
+  </form>
 );
 export default BookForm;
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,10 +1,11 @@
+import BookForm from './BookForm';
 import BookList from './BookList';
 
 const Books = () => (
   <div>
     <h1>Books To Read</h1>
     <BookList />
-    ;
+    <BookForm />
   </div>
 );
 export default Books;
