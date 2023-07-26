6e3c5b214cce47ef7bc2b82aef7b81e61328b635
Alfred Peter <alpeter91@gmail.com>
2023-07-26 23:42:33 +0300

    Add delete book functionality

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 
-const Book = ({ author, title, id }) => (
+const Book = ({
+  author, title, id, remove,
+}) => (
   <div>
     <h3>{title}</h3>
     <h5>{author}</h5>
-    <button type="button" id={id}>
+    <button type="button" id={id} onClick={() => remove(id)}>
       Remove
     </button>
   </div>
@@ -14,5 +16,6 @@ Book.propTypes = {
   author: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
+  remove: PropTypes.func.isRequired,
 };
 export default Book;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,12 +2,23 @@ import Book from './Book';
 import { useTheme } from './BookContext';
 
 const BookList = () => {
-  const { bookCollection } = useTheme();
+  const { bookCollection, setBookCollection } = useTheme();
+
+  const handleRemove = (id) => {
+    const newCollection = bookCollection.filter((book) => book.id !== id);
+    setBookCollection(newCollection);
+  };
 
   return (
     <div>
       {bookCollection.map((item) => (
-        <Book key={item.id} id={item.id} author={item.author} title={item.title} />
+        <Book
+          key={item.id}
+          id={item.id}
+          author={item.author}
+          title={item.title}
+          remove={handleRemove}
+        />
       ))}
     </div>
   );
