da3f0d0495258426d5a19acae6d90c95f0175f8c
Alfred Peter <alpeter91@gmail.com>
2023-07-29 16:02:36 +0300

    Render books from initialState

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,23 +1,17 @@
+import { useSelector } from 'react-redux';
 import Book from './Book';
-import { useBookContext } from './BookContext';
 
 const BookList = () => {
-  const { bookCollection, setBookCollection } = useBookContext();
-
-  const handleRemove = (id) => {
-    const newCollection = bookCollection.filter((book) => book.id !== id);
-    setBookCollection(newCollection);
-  };
+  const { bookArray } = useSelector((store) => store.book);
 
   return (
     <div>
-      {bookCollection.map((item) => (
+      {bookArray.map((item) => (
         <Book
           key={item.id}
           id={item.id}
           author={item.author}
           title={item.title}
-          remove={handleRemove}
         />
       ))}
     </div>
