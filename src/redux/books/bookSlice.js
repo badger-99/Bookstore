b9e95b7e663702dd8d1801b3fb82d9c2e318feec
Alfred Peter <alpeter91@gmail.com>
2023-07-31 20:40:54 +0300

    Add async api call

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,5 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
+
+// const appId = mqojsesd5RNXmiD5UXBK
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/mqojsesd5RNXmiD5UXBK/books';
+export const getBooks = createAsyncThunk(
+  'books/fetchBooks',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios(url);
+      return response.data;
+    } catch (error) {
+      const errorMsg = `${error.code}: ${error.message}`;
+      return thunkAPI.rejectWithValue(errorMsg);
+    }
+  },
+);
 
 const array = [
   {
@@ -44,6 +60,17 @@ const bookSlice = createSlice({
       const newBookArray = store.bookArray.filter((book) => book.id !== bookId);
       store.bookArray = newBookArray;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getBooks.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getBooks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.bookArray = action.payload;
+      })
+      .addCase(getBooks.rejected);
   },
 });
 
