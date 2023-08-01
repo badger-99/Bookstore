0ba2cd08ead83d4e2f203c4941f1bda802c36dc6
Alfred Peter <alpeter91@gmail.com>
2023-08-01 09:54:31 +0300

    Render books from server bug fix

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -6,11 +6,12 @@ import { getBooks } from '../redux/books/bookSlice';
 const BookList = () => {
   const { bookArray, isLoading, error } = useSelector((store) => store.book);
   const dispatch = useDispatch();
+  const keys = Object.keys(bookArray);
 
   useEffect(() => {
     dispatch(getBooks());
-  }, [getBooks]);
-  console.log(isLoading, error);
+  }, []);
+
   if (isLoading) {
     return <h3>Loading...</h3>;
   }
@@ -21,12 +22,12 @@ const BookList = () => {
 
   return bookArray ? (
     <div>
-      {bookArray.map((item) => (
+      {keys.map((key) => (
         <Book
-          key={item.id}
-          id={item.id}
-          author={item.author}
-          title={item.title}
+          key={key}
+          id={key}
+          author={bookArray[key][0].author}
+          title={bookArray[key][0].title}
         />
       ))}
     </div>
