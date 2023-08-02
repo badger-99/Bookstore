3a3d1ba01297b6d546c7ee2f8deec7fa0db0918c
Alfred Peter <alpeter91@gmail.com>
2023-08-02 20:58:58 +0300

    Style progress bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,9 +2,9 @@ import { NavLink } from 'react-router-dom';
 import { ImUser } from 'react-icons/im';
 
 const Navigation = () => (
-  <nav id="navigation">
+  <nav className="navigation row">
     <h1 className="Text-Style-3">Bookstore CMS</h1>
-    <ul>
+    <ul className="row">
       <li>
         <NavLink className="link Text-Style-6" to="/">Books</NavLink>
       </li>
